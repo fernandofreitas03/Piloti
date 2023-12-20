2c1b0c0d749ed4d53f2c0d98fedda6c9a538cce4
fernandofreitas03 <fernandofreitas130@gmail.com>
2023-12-20 12:15:13 -0300

    add  create, update e delete

--- api/noticias.php
+++ api/noticias.php
@@ -6,10 +6,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['logado']) && $_POST['logado'] == 'true') {
 
-        $stmd = $pdo->query('select * from noticias');
-        echo json_encode($stmd->fetch());
+        if (isset($_POST['criar_noticia'])) {
+
+            $createStmt = $pdo->prepare('INSERT INTO noticias (titulo, conteudo) VALUES (?, ?)');
+            $createStmt->execute([$_POST['titulo'], $_POST['conteudo']]);
+
+            echo json_encode(['message' => 'Notícia criada com sucesso']);
+        } elseif (isset($_POST['atualizar_noticia'])) {
+
+            $updateStmt = $pdo->prepare('UPDATE noticias SET titulo = ?, conteudo = ? WHERE id = ?');
+            $updateStmt->execute([$_POST['titulo'], $_POST['conteudo'], $_POST['id']]);
+
+
+            echo json_encode(['message' => 'Notícia atualizada com sucesso']);
+        } elseif (isset($_POST['excluir_noticia'])) {
+            $deleteStmt = $pdo->prepare('DELETE FROM noticias WHERE id = ?');
+            $deleteStmt->execute([$_POST['id']]);
+
+            echo json_encode(['message' => 'Notícia excluída com sucesso']);
+        } else {
+            $stmd = $pdo->query('SELECT * FROM noticias');
+            echo json_encode($stmd->fetch());
+        }
     } else {
         http_response_code(404);
-        echo json_encode(['error' => 'Usuario nao logado']);
+        echo json_encode(['error' => 'Usuário não logado']);
     }
 }
--- api/usuario.php
+++ api/usuario.php
@@ -4,10 +4,26 @@ include_once('../conexao/conexao.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    if (isset($_POST['email']) && isset($_POST['senha'])) {
+    if (isset($_POST['create_user'])) {
 
+        $createStmt = $pdo->prepare('INSERT INTO usuario (USUARIO, EMAIL, CPF, SENHA) VALUES (?, ?, ?, ?)');
+        $createStmt->execute([$_POST['usuario'], $_POST['email'], $_POST['cpf'], $_POST['senha']]);
+
+        echo json_encode(['message' => 'Usuário criado com sucesso']);
+    } elseif (isset($_POST['update_user'])) {
+
+        $updateStmt = $pdo->prepare('UPDATE usuario SET USUARIO = ?, EMAIL = ?, CPF = ?, SENHA = ? WHERE ID = ?');
+        $updateStmt->execute([$_POST['usuario'], $_POST['email'], $_POST['cpf'], $_POST['senha'], $_POST['id']]);
+
+        echo json_encode(['message' => 'Usuário atualizado com sucesso']);
+    } elseif (isset($_POST['delete_user'])) {
+        $deleteStmt = $pdo->prepare('DELETE FROM usuario WHERE ID = ?');
+        $deleteStmt->execute([$_POST['id']]);
+
+        echo json_encode(['message' => 'Usuário excluído com sucesso']);
+    } elseif (isset($_POST['email']) && isset($_POST['senha'])) {
         if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $stmd = $pdo->prepare('select * from usuario where email =?  and senha =?');
+            $stmd = $pdo->prepare('SELECT * FROM usuario WHERE email =? AND senha =?');
             $stmd->execute([$_POST['email'], $_POST['senha']]);
 
             while ($result = $stmd->fetch()) {
@@ -20,8 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 ]);
             }
         } else {
-
-            echo json_encode(['error' => 'Email invalido']);
+            echo json_encode(['error' => 'Email inválido']);
         }
     } else {
         http_response_code(404);
